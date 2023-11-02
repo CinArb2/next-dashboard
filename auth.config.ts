8ad240065fe3bbccc2c418a15f19cdd116a070d1
CinArb2 <cindypao27@gmail.com>
2023-11-02 06:58:05 -0500

    redirect to dashboard if logged in and route is login

--- auth.config.ts
+++ auth.config.ts
@@ -9,9 +9,13 @@ export const authConfig: NextAuthConfig = {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
       const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+      const isLoginUrl = nextUrl.pathname.startsWith('/login');
       if (isOnDashboard) {
         if (isLoggedIn) return true;
         return false; // Redirect unauthenticated users to login page
+      }
+      if (isLoginUrl && isLoggedIn){
+        return Response.redirect(new URL('/dashboard', nextUrl));
       }
       return true;
     },
